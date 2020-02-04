68b42245b61f0041956367e7db9a6342cc86a0b3
Dominika Słowik <dommie73@gmail.com>
2020-02-04 18:20:44 +0100

    Save product-related data to a JSON file & update controllers

--- ex-2-express-basics/models/Product.js
+++ ex-2-express-basics/models/Product.js
@@ -1,23 +1,68 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
 class Product {
 	constructor(name) {
 		this.name = name;
 	}
 
-	save() {
-		products.push(this);
+	static _pathToJson = path.join(
+		path.dirname(process.mainModule.filename),
+		'data',
+		'products.json'
+	);
+
+	static _parseJSONProductData(data) {
+		return JSON.parse(data, (key, value) => {
+			if (Array.isArray(value)) {
+				value.forEach(product => {
+					Product._setProductPrototype(product);
+				});
+			}
+			return value;
+		});
 	}
 
-	static fetchAll() {
+	static _setProductPrototype(product) {
+		return Object.setPrototypeOf(product, Product.prototype);
+	}
+
+	static async _readFile() {
+		const products = await new Promise((resolve, reject) => {
+			return fs.readFile(Product._pathToJson, (error, data) => {
+				if (error) {
+					return reject(error);
+				}
+				return resolve(Product._parseJSONProductData(data));
+			});
+		});
+
 		return products;
 	}
-}
 
-products.push(
-	new Product('product is null'),
-	new Product('undefined product'),
-	new Product('product is not defined')
-);
+	static async _writeFile(data) {
+		return new Promise((resolve, reject) => {
+			return fs.writeFile(
+				Product._pathToJson,
+				JSON.stringify(data, null, 2),
+				error => {
+					if (error) {
+						return reject(error);
+					}
+					return resolve(`Saved to ${this.pathToJson}`);
+				}
+			);
+		});
+	}
+
+	static async fetchAll() {
+		return await Product._readFile();
+	}
+
+	async save() {
+		const products = await Product._readFile();
+		await Product._writeFile(products.concat(this));
+	}
+}
 
 module.exports = Product;
