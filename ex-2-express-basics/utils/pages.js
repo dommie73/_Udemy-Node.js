06fa796ec165e348429f27825bfaa879371b4d07
Dominika Słowik <dommie73@gmail.com>
2020-02-05 02:10:06 +0100

    Add items to the navigation

--- ex-2-express-basics/utils/pages.js
+++ ex-2-express-basics/utils/pages.js
@@ -1,6 +1,9 @@
 const pages = [
-	{ title: 'Shop', url: '/' },
-	{ title: 'Add Product', url: '/admin/add-product' }
+	{ title: 'Home', url: '/' },
+	{ title: 'Products', url: '/products' },
+	{ title: 'Cart', url: '/cart' },
+	{ title: 'Add Product', url: '/admin/add-product' },
+	{ title: 'Admin Products', url: '/admin/products' }
 ];
 
 module.exports = pages;
