5d87d7a8bacdcd06096857edd4790ddaf5066659
Dominika Słowik <dommie73@gmail.com>
2020-02-06 01:53:47 +0100

    Make `products-list` reusable [admin & shop]

--- ex-2-express-basics/controllers/admin/getProducts.js
+++ ex-2-express-basics/controllers/admin/getProducts.js
@@ -2,7 +2,7 @@ const Product = require('../../models/Product');
 
 const getProducts = async (req, res) => {
 	const products = await Product.fetchAll();
-	res.render('admin/products-list', { pageTitle: 'Admin Products', products });
+	res.render('shop/products-list', { pageTitle: 'Admin Products', products });
 };
 
 module.exports = getProducts;
