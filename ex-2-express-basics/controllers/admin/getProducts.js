7bea0b9ca03ff7c5e9b8a583d8a70237a0bc9a90
Dominika Słowik <dommie73@gmail.com>
2020-02-06 01:19:15 +0100

    Update & style `products-list` views

--- ex-2-express-basics/controllers/admin/getProducts.js
+++ ex-2-express-basics/controllers/admin/getProducts.js
@@ -1,5 +1,8 @@
-const getProducts = (req, res) => {
-	res.render('admin/products-list', { pageTitle: 'Admin Products' });
+const Product = require('../../models/Product');
+
+const getProducts = async (req, res) => {
+	const products = await Product.fetchAll();
+	res.render('admin/products-list', { pageTitle: 'Admin Products', products });
 };
 
 module.exports = getProducts;
