2aeaefc191c4b1ee56f2f0c3f1c60687aae93333
Dominika Słowik <dommie73@gmail.com>
2020-02-16 23:48:56 +0100

    Move file-related logic to `JSONFileManager` class & update `Product` model

--- ex-2-express-basics/models/Product.js
+++ ex-2-express-basics/models/Product.js
@@ -1,76 +1,42 @@
-const fs = require('fs');
 const path = require('path');
 
-class Product {
+const JSONFileManager = require('./JSONFileManager');
+
+class Product extends JSONFileManager {
+	static _pathToJson = path.join('data', 'products.json');
+
 	constructor(name, imageUrl, price, description) {
+		super();
 		this.name = name;
 		this.imageUrl = imageUrl;
 		this.price = price;
 		this.description = description;
 	}
 
-	static _pathToJson = path.join(
-		path.dirname(process.mainModule.filename),
-		'data',
-		'products.json'
-	);
-
-	static _parseJSONProductData(data) {
+	static parseJSONData(data) {
 		return JSON.parse(data, (key, value) => {
 			if (Array.isArray(value)) {
 				value.forEach(product => {
-					Product._setProductPrototype(product);
+					Object.setPrototypeOf(product, this.prototype);
 				});
 			}
 			return value;
 		});
 	}
 
-	static _setProductPrototype(product) {
-		return Object.setPrototypeOf(product, Product.prototype);
-	}
-
-	static async _readFile() {
-		const products = await new Promise((resolve, reject) => {
-			return fs.readFile(Product._pathToJson, (error, data) => {
-				if (error) {
-					return reject(error);
-				}
-				return resolve(Product._parseJSONProductData(data));
-			});
-		});
-
-		return products;
-	}
-
-	static async _writeFile(data) {
-		return new Promise((resolve, reject) => {
-			return fs.writeFile(
-				Product._pathToJson,
-				JSON.stringify(data, null, 2),
-				error => {
-					if (error) {
-						return reject(error);
-					}
-					return resolve(`Saved to ${Product._pathToJson}`);
-				}
-			);
-		});
-	}
-
 	static async fetchAll() {
-		return await Product._readFile();
+		return await this.readFile();
 	}
 
 	static async fetchById(id) {
-		const products = await Product._readFile();
+		const products = await this.readFile();
 		return products.find(product => product.id === id);
 	}
 
 	async save() {
 		this.id = Math.floor(Math.random() * 10 ** 8).toString();
-		const products = await Product._readFile();
-		await Product._writeFile(products.concat(this));
+		const products = await Product.readFile();
+		await Product.writeFile(products.concat({ ...this, price: +this.price }));
 	}
 }
 
