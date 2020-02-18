93f40ffc54d5164f49712adb94b8e06816927de2
Dominika Słowik <dommie73@gmail.com>
2020-02-18 23:43:21 +0100

    Fill the `edit-product` form with existing data if in editing mode

--- ex-2-express-basics/controllers/admin/getEditProductForm.js
+++ ex-2-express-basics/controllers/admin/getEditProductForm.js
@@ -1,12 +1,15 @@
-const getEditProductForm = (req, res) => {
+const { Product } = require('../../models');
+
+const getEditProductForm = async (req, res) => {
 	const { id } = req.params;
 	const { edit } = req.query;
 
 	if (edit === 'true') {
+		const product = await Product.fetchById(id);
 		res.render('admin/edit-product', {
 			pageTitle: 'Edit Product',
-			productId: id,
-			editing: true
+			editing: true,
+			product
 		});
 	} else {
 		res.redirect(`/products/${id}`);
