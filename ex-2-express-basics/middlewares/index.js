e0709b4c0deaaadffa54634dfd800920630277dc
Dominika Słowik <dommie73@gmail.com>
2020-02-24 21:19:47 +0100

    Create a dummy user in the database & register `user` middleware

--- ex-2-express-basics/middlewares/index.js
+++ ex-2-express-basics/middlewares/index.js
@@ -1,4 +1,5 @@
 const currentUrl = require('./currentUrl');
 const reqLogger = require('./reqLogger');
+const user = require('./user');
 
-module.exports = { currentUrl, reqLogger };
+module.exports = { currentUrl, reqLogger, user };
