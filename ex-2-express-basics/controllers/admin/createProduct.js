126ad63b3634e6a6bb8fedf67df5bb09fe4db69e
Dominika Słowik <dommie73@gmail.com>
2020-02-24 21:36:56 +0100

    Associate the user with a newly created product instance

--- ex-2-express-basics/controllers/admin/createProduct.js
+++ ex-2-express-basics/controllers/admin/createProduct.js
@@ -1,8 +1,7 @@
-const Product = require('../../models/Product');
-
 const createProduct = async (req, res) => {
+	const { user } = req;
 	const { name, imageUrl, price, description } = req.body;
-	await Product.create({ name, imageUrl, price, description });
+	await user.createProduct({ name, imageUrl, price, description });
 	res.redirect('/admin/products');
 };
 
