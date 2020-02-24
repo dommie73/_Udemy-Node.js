8befa826bff8aa7a5194d40d508be35c0b135d08
Dominika Słowik <dommie73@gmail.com>
2020-02-24 14:17:19 +0100

    Handle inserting & retrieving products

--- ex-2-express-basics/controllers/admin/createProduct.js
+++ ex-2-express-basics/controllers/admin/createProduct.js
@@ -2,8 +2,7 @@ const Product = require('../../models/Product');
 
 const createProduct = async (req, res) => {
 	const { name, imageUrl, price, description } = req.body;
-	const product = new Product(name, imageUrl, price, description);
-	await product.save();
+	await Product.create({ name, imageUrl, price, description });
 	res.redirect('/admin/products');
 };
 
--- ex-2-express-basics/controllers/admin/getProducts.js
+++ ex-2-express-basics/controllers/admin/getProducts.js
@@ -1,7 +1,7 @@
 const Product = require('../../models/Product');
 
 const getProducts = async (req, res) => {
-	const products = await Product.fetchAll();
+	const products = await Product.findAll();
 	res.render('shop/products-list', { pageTitle: 'Admin Products', products });
 };
 
