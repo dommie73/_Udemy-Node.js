00b8d5fc8be6d4dc6350e75642ac3e1fa496e023
Dominika Słowik <dommie73@gmail.com>
2020-02-26 22:51:46 +0100

    Display orders on the `/orders` page

--- ex-2-express-basics/controllers/shop/getOrders.js
+++ ex-2-express-basics/controllers/shop/getOrders.js
@@ -1,5 +1,8 @@
-const getOrders = (req, res) => {
-	res.render('shop/orders', { pageTitle: 'Orders' });
+const getOrders = async (req, res) => {
+	const { user } = req;
+	const orders = await user.getOrders({ include: ['Products'] });
+
+	res.render('shop/orders', { pageTitle: 'Orders', orders });
 };
 
 module.exports = getOrders;
