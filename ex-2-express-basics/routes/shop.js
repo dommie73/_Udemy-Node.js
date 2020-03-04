926a98342fe5af6fd57697081a06bc9224b80adf
Dominika Słowik <dommie73@gmail.com>
2020-03-04 21:49:48 +0100

    Display the cart

--- ex-2-express-basics/routes/shop.js
+++ ex-2-express-basics/routes/shop.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 
 const addToCart = require('../controllers/shop/addToCart');
+const getCart = require('../controllers/shop/getCart');
 const getIndex = require('../controllers/shop/getIndex');
 const getProducts = require('../controllers/shop/getProducts');
 const getProductDetails = require('../controllers/shop/getProductDetails');
@@ -8,7 +9,7 @@ const getProductDetails = require('../controllers/shop/getProductDetails');
 const router = Router();
 
 router.get('/', getIndex);
-// router.get('/cart', shopControllers.getCart);
+router.get('/cart', getCart);
 router.post('/cart', addToCart);
 // router.post('/cart/delete-product', shopControllers.deleteFromCart);
 // router.get('/orders', shopControllers.getOrders);
