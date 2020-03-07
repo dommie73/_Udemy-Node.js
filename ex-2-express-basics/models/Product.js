021ad2ef2fbb3ecddc917024eeb49d171531a3b0
Dominika Słowik <dommie73@gmail.com>
2020-03-07 22:12:53 +0100

    Define `Product` schema & model

--- ex-2-express-basics/models/Product.js
+++ ex-2-express-basics/models/Product.js
@@ -1,40 +1,18 @@
-const { ObjectId } = require('mongodb');
+const { model, Schema } = require('mongoose');
 
-const mongo = require('../database');
+const productSchema = new Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	imageUrl: String,
+	price: {
+		type: Number,
+		required: true
+	},
+	description: String
+});
 
-class Product {
-	constructor(name, imageUrl, price, description, id, userId) {
-		this.name = name;
-		this.imageUrl = imageUrl;
-		this.price = +price;
-		this.description = description;
-		this._id = id ? ObjectId(id) : null;
-		this.userId = userId ? ObjectId(userId) : null;
-	}
-
-	static deleteById(id) {
-		return mongo.db.collection('products').deleteOne({ _id: ObjectId(id) });
-	}
-
-	static fetchAll(query = {}) {
-		return mongo.db
-			.collection('products')
-			.find(query)
-			.toArray();
-	}
-
-	static fetchById(id) {
-		return mongo.db.collection('products').findOne({ _id: ObjectId(id) });
-	}
-
-	save() {
-		if (this._id) {
-			return mongo.db
-				.collection('products')
-				.updateOne({ _id: this._id }, { $set: this });
-		}
-		return mongo.db.collection('products').insertOne(this);
-	}
-}
+const Product = model('Product', productSchema);
 
 module.exports = Product;
