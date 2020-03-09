f4ab32a4c112fae6e4b88655b4cbbcd5fc23f826
Dominika Słowik <dommie73@gmail.com>
2020-03-09 22:51:56 +0100

    Filter products on `/products` page - display only the products that are unassociated with the current user
    [user's products remain visible under `/admin/products`]

--- ex-2-express-basics/controllers/shop/getProducts.js
+++ ex-2-express-basics/controllers/shop/getProducts.js
@@ -1,7 +1,8 @@
 const { Product } = require('../../models');
 
 const getProducts = async (req, res) => {
-	const products = await Product.find();
+	const { user } = req;
+	const products = await Product.find({ userId: { $ne: user._id } });
 
 	res.render('shop/products-list', { pageTitle: 'Products', products });
 };
