c27c3c96edbd3da9dd25d80dfcebedcebe1a6942
Dominika Słowik <dommie73@gmail.com>
2020-03-12 23:48:38 +0100

    Display all products on `/products` page if the user is not logged in

--- ex-2-express-basics/controllers/shop/getProducts.js
+++ ex-2-express-basics/controllers/shop/getProducts.js
@@ -2,7 +2,9 @@ const { Product } = require('../../models');
 
 const getProducts = async (req, res) => {
 	const { user } = req;
-	const products = await Product.find({ userId: { $ne: user._id } });
+	const products = await Product.find(
+		user ? { userId: { $ne: user._id } } : {}
+	);
 
 	res.render('shop/products-list', { pageTitle: 'Products', products });
 };
