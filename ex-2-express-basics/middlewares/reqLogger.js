15d3d46a4ded9f83e75e8efd535120a719bf4370
Dominika Słowik <dommie73@gmail.com>
2020-03-15 21:57:30 +0100

    Enhance `reqLogger` middleware

--- ex-2-express-basics/middlewares/reqLogger.js
+++ ex-2-express-basics/middlewares/reqLogger.js
@@ -1,7 +1,13 @@
 const chalk = require('chalk');
 
 const reqLogger = (req, res, next) => {
-	console.log(chalk.blue(`requested: ${chalk.bold(req.path)}`));
+	console.log(
+		chalk.blue(
+			`requested: ${chalk.bold(req.path)}\n` +
+				`  - method: ${chalk.bold(req.method)},\n` +
+				`  - user: ${chalk.bold(req.user && req.user.email)}`
+		)
+	);
 	next();
 };
 
