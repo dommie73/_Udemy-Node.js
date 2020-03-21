baf83be9e852877fce4e93fe10e1f4826f483d8c
Dominika Słowik <dommie73@gmail.com>
2020-03-21 01:23:38 +0100

    Verify the password reset token
    [if the token is invalid or expired, redirect to `/reset`]

--- ex-2-express-basics/controllers/auth/getNewPasswordForm.js
+++ ex-2-express-basics/controllers/auth/getNewPasswordForm.js
@@ -1,7 +1,22 @@
-const getNewPasswordForm = (req, res) => {
-	const { token } = req.params;
+const { User } = require('../../models');
 
-	res.render('auth/new-password', { pageTitle: 'Set a new password' });
+const getNewPasswordForm = async (req, res) => {
+	const { token } = req.params;
+	const user = await User.findOne({
+		'passwordResetToken.value': token,
+		'passwordResetToken.expires': { $gt: Date.now() }
+	});
+
+	if (user) {
+		return res.render('auth/new-password', {
+			pageTitle: 'Set a new password',
+			userId: user._id.toString(),
+			token
+		});
+	}
+
+	req.flash('error', 'This password reset token is invalid.');
+	res.redirect('/reset');
 };
 
 module.exports = getNewPasswordForm;
