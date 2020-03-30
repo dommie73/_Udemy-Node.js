77f94cb7d11f15d6e42423b8d4a2aba364a6c98b
Dominika Słowik <dommie73@gmail.com>
2020-03-31 01:30:40 +0200

    Catch errors in `auth` controllers

--- ex-2-express-basics/controllers/auth/logout.js
+++ ex-2-express-basics/controllers/auth/logout.js
@@ -1,5 +1,8 @@
 const logout = (req, res, next) => {
-	req.session.destroy(() => {
+	req.session.destroy(err => {
+		if (err) {
+			next(err);
+		}
 		/* 
 			req.flash() cannot be used here because the session has just been destroyed. 
 			A cookie is used instead.
