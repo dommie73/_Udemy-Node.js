ee4f29cf4dae6785a1ac03995e312ce5cf73eaba
Dominika Słowik <dommie73@gmail.com>
2020-04-04 23:54:19 +0200

    Apply image uploading functionality to POST `/admin/edit-product` route

--- ex-2-express-basics/routes/admin.js
+++ ex-2-express-basics/routes/admin.js
@@ -7,20 +7,23 @@ const { imageFilter } = require('../utils/multer');
 
 const router = Router();
 
+const imageLimits = {
+	fileSize: 1 * 1024 * 1024
+};
+
 router.all('*', protected);
 
 router.get('/add-product', adminControllers.getAddProductForm);
 router.post(
 	'/add-product',
-	upload('image', imageFilter, {
-		fileSize: 1 * 1024 * 1024
-	}),
+	upload('image', imageFilter, imageLimits),
 	adminValidators.updateProduct,
 	adminControllers.createProduct
 );
 router.get('/edit-product/:id', adminControllers.getEditProductForm);
 router.post(
 	'/edit-product',
+	upload('image', imageFilter, imageLimits),
 	adminValidators.updateProduct,
 	adminControllers.updateProduct
 );
