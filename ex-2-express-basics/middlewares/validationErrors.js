f8bfc3a2e6dab524e83f0a9c143a4bc8c4bda66d
Dominika Słowik <dommie73@gmail.com>
2020-04-04 22:38:09 +0200

    Remove/save the uploaded image, depending on the validation results

--- ex-2-express-basics/middlewares/validationErrors.js
+++ ex-2-express-basics/middlewares/validationErrors.js
@@ -1,19 +1,40 @@
+const path = require('path');
+
 const { validationResult } = require('express-validator');
+const { move, remove } = require('fs-extra');
 
-const validationErrors = (req, res, next) => {
-	const errors = validationResult(req);
+const { rootDir } = require('../utils/helpers');
 
-	if (!errors.isEmpty()) {
-		req.flash(
-			'error',
-			errors.array({ onlyFirstError: true }).map(error => error.msg)
-		);
-		req.inputs.set();
+const validationErrors = async (req, res, next) => {
+	try {
+		const errors = validationResult(req);
+		const { file } = req;
 
-		return req.saveSessionAndRedirect('back');
+		if (!errors.isEmpty()) {
+			if (file) {
+				await remove(file.path);
+			}
+
+			req.flash(
+				'error',
+				errors.array({ onlyFirstError: true }).map(error => error.msg)
+			);
+			req.inputs.set();
+
+			return req.saveSessionAndRedirect('back');
+		}
+
+		if (file) {
+			await move(
+				file.path,
+				path.join(rootDir, 'public', 'uploads', file.filename)
+			);
+		}
+
+		next();
+	} catch (err) {
+		next(err);
 	}
-
-	next();
 };
 
 module.exports = validationErrors;
