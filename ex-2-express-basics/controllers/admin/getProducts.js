1c8be093c7582087f157a263fd1d7c94aa1ba3e8
Dominika Słowik <dommie73@gmail.com>
2020-04-10 22:55:43 +0200

    Use pagination in `getProducts` controllers

--- ex-2-express-basics/controllers/admin/getProducts.js
+++ ex-2-express-basics/controllers/admin/getProducts.js
@@ -3,9 +3,18 @@ const { Product } = require('../../models');
 const getProducts = async (req, res, next) => {
 	try {
 		const { user } = req;
-		const products = await Product.find({ userId: user });
+		const currentPage = +req.query.page || 1;
+		const { products, totalPages } = await Product.paginate(
+			{ userId: user },
+			{ currentPage }
+		);
 
-		res.render('shop/products-list', { pageTitle: 'Admin Products', products });
+		res.render('shop/products-list', {
+			pageTitle: 'Admin Products',
+			products,
+			currentPage,
+			totalPages
+		});
 	} catch (err) {
 		next(err);
 	}
--- ex-2-express-basics/controllers/shop/getProducts.js
+++ ex-2-express-basics/controllers/shop/getProducts.js
@@ -3,11 +3,18 @@ const { Product } = require('../../models');
 const getProducts = async (req, res, next) => {
 	try {
 		const { user } = req;
-		const products = await Product.find(
-			user ? { userId: { $ne: user._id } } : {}
+		const currentPage = +req.query.page || 1;
+		const { products, totalPages } = await Product.paginate(
+			user ? { userId: { $ne: user._id } } : {},
+			{ currentPage }
 		);
 
-		res.render('shop/products-list', { pageTitle: 'Products', products });
+		res.render('shop/products-list', {
+			pageTitle: 'Products',
+			products,
+			currentPage,
+			totalPages
+		});
 	} catch (err) {
 		next(err);
 	}
