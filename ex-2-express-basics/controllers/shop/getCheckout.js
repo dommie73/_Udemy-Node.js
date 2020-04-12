9789d5cff92b4a91a9a5db9a0a4cef9a8ec7f0f8
Dominika Słowik <dommie73@gmail.com>
2020-04-12 22:10:53 +0200

    Update `getCheckout` controller

--- ex-2-express-basics/controllers/shop/getCheckout.js
+++ ex-2-express-basics/controllers/shop/getCheckout.js
@@ -1,5 +1,12 @@
-const getCheckout = (req, res) => {
-	res.render('shop/checkout', { pageTitle: 'Checkout' });
+const getCheckout = async (req, res, next) => {
+	try {
+		const { user } = req;
+		const cart = await user.getCart();
+
+		res.render('shop/checkout', { pageTitle: 'Checkout', cart });
+	} catch (err) {
+		next(err);
+	}
 };
 
 module.exports = getCheckout;
