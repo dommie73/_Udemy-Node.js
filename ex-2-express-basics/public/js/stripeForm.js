025cb19f714f89984b13181af93452aeb7e50d8b
Dominika Słowik <dommie73@gmail.com>
2020-04-14 23:34:14 +0200

    Catch errors from each Stripe element & display them in the UI

--- ex-2-express-basics/public/js/stripeForm.js
+++ ex-2-express-basics/public/js/stripeForm.js
@@ -2,28 +2,48 @@
 	const stripe = Stripe(publishableKey);
 	const elements = stripe.elements({
 		fonts: [
-			{
-				cssSrc: 'https://fonts.googleapis.com/css?family=Poppins&display=swap'
-			}
+			{ cssSrc: 'https://fonts.googleapis.com/css?family=Poppins&display=swap' }
 		]
 	});
 
+	const elementClasses = {
+		complete: 'valid',
+		focus: 'focused',
+		invalid: 'invalid'
+	};
 	const computedLabelStyle = window.getComputedStyle(
 		document.querySelector('.form-group__label')
 	);
-
-	const style = {
+	const elementStyles = {
 		base: {
 			fontFamily: computedLabelStyle.getPropertyValue('font-family'),
 			fontSize: computedLabelStyle.getPropertyValue('font-size')
+		},
+		invalid: {
+			color: computedLabelStyle.getPropertyValue('color')
 		}
 	};
 
-	const cardNumber = elements
-		.create('cardNumber', { style })
-		.mount('#card-number');
-	const cardExpiry = elements
-		.create('cardExpiry', { style })
-		.mount('#card-expiry');
-	const cardCvc = elements.create('cardCvc', { style }).mount('#card-cvc');
+	class StripeElement {
+		constructor(elementType, elementSelector) {
+			this.error = new ErrorElement();
+			this.element = elements.create(elementType, {
+				classes: elementClasses,
+				style: elementStyles
+			});
+
+			this.element.mount(elementSelector);
+			this._listenForErrors();
+		}
+
+		_listenForErrors() {
+			this.element.on('change', event => {
+				event.error ? this.error.show(event.error.message) : this.error.hide();
+			});
+		}
+	}
+
+	const cardNumber = new StripeElement('cardNumber', '#card-number');
+	const cardExpiry = new StripeElement('cardExpiry', '#card-expiry');
+	const cardCvc = new StripeElement('cardCvc', '#card-cvc');
 })();
