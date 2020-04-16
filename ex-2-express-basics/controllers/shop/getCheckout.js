73a20917768207675e44ad8d8807f51a4cc0c8b7
Dominika Słowik <dommie73@gmail.com>
2020-04-16 20:59:35 +0200

    Redirect from `/checkout` to `/cart` if the cart is empty

--- ex-2-express-basics/controllers/shop/getCheckout.js
+++ ex-2-express-basics/controllers/shop/getCheckout.js
@@ -3,6 +3,10 @@ const getCheckout = async (req, res, next) => {
 		const { user } = req;
 		const cart = await user.getCart();
 
+		if (cart.products.length === 0) {
+			return res.redirect('/cart');
+		}
+
 		res.render('shop/checkout', { pageTitle: 'Checkout', cart });
 	} catch (err) {
 		next(err);
