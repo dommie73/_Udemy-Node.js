36ef6fd3e877dc88c0a7f4236c7dd83b48a4f6af
Dominika Słowik <dommie73@gmail.com>
2020-08-04 21:52:01 +0200

    Install `express-validator` & create `validationErrors` middleware

--- ex-3-rest-basics/server/middlewares/index.js
+++ ex-3-rest-basics/server/middlewares/index.js
@@ -1,9 +1,11 @@
 const cors = require('./cors');
 const errorHandler = require('./errorHandler');
 const notFoundHandler = require('./notFoundHandler');
+const validationErrors = require('./validationErrors');
 
 module.exports = {
 	cors,
 	errorHandler,
-	notFoundHandler
+	notFoundHandler,
+	validationErrors
 };
