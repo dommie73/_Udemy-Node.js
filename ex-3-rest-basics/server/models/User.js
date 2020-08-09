89984357575a33626396ce69734572299b0e3dbf
Dominika Słowik <dommie73@gmail.com>
2020-08-09 16:01:43 +0200

    Add `User` model

---
+++ ex-3-rest-basics/server/models/User.js
@@ -0,0 +1,44 @@
+const { model, Schema, Types } = require('mongoose');
+const { hash } = require('bcryptjs');
+
+const userSchema = new Schema(
+	{
+		email: {
+			type: String,
+			required: true
+		},
+		password: {
+			type: String,
+			required: true,
+			select: false
+		},
+		name: {
+			type: String,
+			required: true
+		},
+		status: {
+			type: String,
+			default: 'I am new!'
+		},
+		posts: [
+			{
+				type: Types.ObjectId,
+				ref: 'Post'
+			}
+		]
+	},
+	{ timestamps: true }
+);
+
+userSchema.pre('save', async function (next) {
+	if (this.isModified('password')) {
+		this.password = await hash(this.password, 12);
+	}
+	next();
+});
+
+userSchema.set('toJSON', { useProjection: true });
+
+const User = model('User', userSchema);
+
+module.exports = User;
