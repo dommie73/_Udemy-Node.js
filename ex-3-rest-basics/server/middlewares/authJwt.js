98de0a606cd4a8e906958f535e7738c4f9f4e511
Dominika Słowik <dommie73@gmail.com>
2020-08-28 23:59:15 +0200

    Test GET `/auth` endpoint & fix `authJwt` middleware

--- ex-3-rest-basics/server/middlewares/authJwt.js
+++ ex-3-rest-basics/server/middlewares/authJwt.js
@@ -5,8 +5,15 @@ const ErrorHandler = require('../utils/ErrorHandler');
 
 const authJwt = async (req, res, next) => {
 	try {
-		const token = req.get('Authorization').split(' ')[1];
-		const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+		let decodedToken;
+
+		try {
+			const token = req.get('Authorization').split(' ')[1];
+			decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+		} catch {
+			throw new ErrorHandler(401, 'Invalid credentials.');
+		}
+
 		const user = await User.findById(decodedToken.userId).orFail(
 			new ErrorHandler(404, 'User not found')
 		);
@@ -14,7 +21,7 @@ const authJwt = async (req, res, next) => {
 		req.user = user;
 		next();
 	} catch (err) {
-		throw new ErrorHandler(401, 'Invalid credentials.');
+		next(err);
 	}
 };
 
